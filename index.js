336d5b12fe63841ec6207a4ad46166d793c1365a
wambidaniel <68166469+wambidaniel@users.noreply.github.com>
2022-12-04 01:03:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function increment{
+    var number = Number(document.getElementById('number').innerHTML)
+
+    document.getElementById('number').innerHTML = number + 1
+}
+function decrement{
+
+    var number = Number(document.getElementById('number').innerHTML)
+
+    document.getElementById('number').innerHTML = number - 1
+
+}
+function reset{
+    document.getElementById('number').innerHTML = 0
+
+
+}
